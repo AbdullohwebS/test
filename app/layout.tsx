48d9aa2024f4a62a8c26196437e554d9de32b066
Abdulloh Nurmukhammedov <158317198+AbdullohwebS@users.noreply.github.com>
2025-06-14 13:35:12 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Cars Json',
+  description: 'Cars Json',
+  generator: 'Cars Json',
 }
 
 export default function RootLayout({
